c8d3c9d1b040f0ca2a16bc41d6281f98d80f275a
Genadi Kupreishvili <genadikupreishvili@gmail.com>
2024-03-24 23:45:03 +0400

    create templates: main, home, navbar, room

--- GenHub/base/views.py
+++ GenHub/base/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
-# Create your views here.
+rooms = [{"id":1, "name": "lets learn python"},
+         {"id":1, "name": "ldesign with me"},
+         {"id":1, "name": "frontend feveloper"},
+         ]
+
+def home(request):
+    return render(request, "home.html", {"rooms":rooms})
+
+def room(request):
+    return render(request, "room.html")
