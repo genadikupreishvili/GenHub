b8277ddc7ed1f8b15e62d30ba8fb7ece95c67db0
Genadi Kupreishvili <genadikupreishvili@gmail.com>
2024-03-27 18:00:33 +0400

    modified:   GenHub/base/__pycache__/views.cpython-310.pyc         modified:   GenHub/base/views.py         deleted:    GenHub/templates/home.html         deleted:    GenHub/templates/room.html

--- GenHub/base/views.py
+++ GenHub/base/views.py
@@ -6,7 +6,8 @@ rooms = [{"id":1, "name": "lets learn python"},
          ]
 
 def home(request):
-    return render(request, "home.html", {"rooms":rooms})
+    context = {"rooms":rooms}
+    return render(request, "base/home.html", context)
 
 def room(request):
-    return render(request, "room.html")
+    return render(request, "base/room.html")
