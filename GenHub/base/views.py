d50b11981e45a4aa23784dbe91f30938ba2bd196
Genadi Kupreishvili <genadikupreishvili@gmail.com>
2024-03-27 18:30:38 +0400

    modified:   GenHub/base/__pycache__/urls.cpython-310.pyc         modified:   GenHub/base/__pycache__/views.cpython-310.pyc         modified:   GenHub/base/templates/base/home.html         modified:   GenHub/base/templates/base/room.html         modified:   GenHub/base/urls.py         modified:   GenHub/base/views.py

--- GenHub/base/views.py
+++ GenHub/base/views.py
@@ -1,13 +1,18 @@
 from django.shortcuts import render
 
 rooms = [{"id":1, "name": "lets learn python"},
-         {"id":1, "name": "ldesign with me"},
-         {"id":1, "name": "frontend feveloper"},
+         {"id":2, "name": "ldesign with me"},
+         {"id":3, "name": "frontend feveloper"},
          ]
 
-def home(request):
+def home(request, ):
     context = {"rooms":rooms}
     return render(request, "base/home.html", context)
 
-def room(request):
-    return render(request, "base/room.html")
+def room(request, pk):
+    room = None
+    for i in rooms:
+        if i['id'] == int(pk):
+            room = i
+    context = {'room':room}
+    return render(request, "base/room.html", context)
