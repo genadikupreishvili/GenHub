ad2587b1b2530c7b8bdb9a1a35fae3497cdf8311
Genadi Kupreishvili <genadikupreishvili@gmail.com>
2024-04-07 17:05:01 +0400

    Add chat room delete functionality, add participants

--- GenHub/base/views.py
+++ GenHub/base/views.py
@@ -76,6 +76,7 @@ def home(request):
 def room(request, pk):
     room = Room.objects.get(id = pk)
     room_messages = room.message_set.all().order_by('-created')
+    participants =  room.participants.all() 
 
     if request.method == 'POST':
         message = Message.objects.create(
@@ -83,11 +84,12 @@ def room(request, pk):
             room = room,
             body = request.POST.get('body')
         )
+        room.participants.add(request.user)
         return redirect('room', pk = room.id)
 
 
 
-    context = {'room':room, "room_messages":room_messages}
+    context = {'room':room, "room_messages":room_messages, 'participants':participants}
     return render(request, "base/room.html", context)
 
 @login_required(login_url= 'login')
@@ -130,3 +132,17 @@ def deleteRoom(request, pk ):
         return redirect("home")
 
     return render(request, "base/delete.html", {"obj":room})
+
+
+@login_required(login_url= 'login')
+def deleteMessage(request, pk ):
+    message =Message.objects.get(id = pk)
+
+    if request.user != message.user  :
+        return  HttpResponse("You are not allowed here! ")
+    
+    if request.method == 'POST':
+        message.delete()
+        return redirect("home")
+
+    return render(request, "base/delete.html", {"obj": message})
