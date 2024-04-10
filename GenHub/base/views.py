6dac236737b99f1f2517d8fd5063621d8cadf0bd
Genadi Kupreishvili <genadikupreishvili@gmail.com>
2024-04-10 20:46:21 +0400

    Add static files

--- GenHub/base/views.py
+++ GenHub/base/views.py
@@ -71,8 +71,10 @@ def home(request):
     topics = Topic.objects.all()
     room_count = rooms.count()
     room_messages = Message.objects.filter(Q(room__topic__name__icontains=q))
+    users = User.objects.all()
 
-    context = {"rooms":rooms, 'topics': topics, "room_count":room_count, 'room_messages':room_messages }
+
+    context = {"rooms":rooms, 'topics': topics, "room_count":room_count, 'room_messages':room_messages,  'users': users }
     return render(request, "base/home.html", context)
 
 def room(request, pk):
@@ -94,6 +96,7 @@ def room(request, pk):
     context = {'room':room, "room_messages":room_messages, 'participants':participants}
     return render(request, "base/room.html", context)
 
+
 def userProfile(request, pk):
     user = User.objects.get(id = pk) 
     rooms = user.room_set.all() 
@@ -102,13 +105,16 @@ def userProfile(request, pk):
     context = {'user': user, 'rooms':rooms, 'room_messages':room_messages, "topics": topics}
     return render(request, 'base/profile.html', context)
 
+
 @login_required(login_url= 'login')
 def createRoom(request):
     form = RoomForm()
     if request.method == 'POST':
         form = RoomForm(request.POST)
         if form.is_valid():
-             form.save()
+             room = form.save(commit=False)
+             room.host = request.user
+             room.save()
              return redirect("home")
     context = {"form" : form}
     return render(request, 'base/room_form.html', context )
