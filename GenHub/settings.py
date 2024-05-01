51f49b9adc7ea332bf92e4bcafe7834c4eead195
Genadi Kupreishvili <genadikupreishvili@gmail.com>
2024-05-02 01:05:15 +0400

    modify static.py

--- GenHub/settings.py
+++ GenHub/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-vjd6ic!(!4quonq060no8sntp+kb+ymxcdf)%mt5l@z^w)(30u
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://genhub-kskx.onrender.com']
+ALLOWED_HOSTS = ['genhub-kskx.onrender.com']
 
 
 # Application definition
@@ -136,9 +136,8 @@ STATICFILES_DIRS = [
 ]
 
 MEDIA_ROOT = BASE_DIR / 'static/images' 
-MEDIA_ROOT = BASE_DIR / "static/images"
 
-STATIC_ROOT = BASE_DIR / "static"
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 
 
